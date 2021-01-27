b787067df6d6d35a10254d26ebbfbd1a5e2d0f26
Michael Parsons <mparsons@credengine.onmicrosoft.com>
2021-01-27 09:00:58 -0600

    Enable publishing a list in either plain class or JSON-LD formatte class;

--- src/RA.Models/Input/OccupationRequest.cs
+++ src/RA.Models/Input/OccupationRequest.cs
@@ -17,6 +17,26 @@ namespace RA.Models.Input
 		/// </summary>
 		public Occupation Occupation { get; set; } = new Occupation();
 
+		/// <summary>
+		/// Occupation already formatted as JSON-LD
+		/// ONLY USED WITH PUBLISH LIST
+		/// </summary>
+		public MJ.Occupation FormattedOccupation { get; set; } = new MJ.Occupation();
+	}
+	/// <summary>
+	/// Request class for publishing a list of Occupations
+	/// </summary>
+	public class OccupationListRequest : BaseRequest
+	{
+		/// <summary>
+		/// List of Occupations
+		/// </summary>
+		public List<object> OccupationList { get; set; } = new List<object>();
+		/// <summary>
+		/// HasLanguageMaps
+		/// If false, will format input using the plain Task classes otherwise the JSON-LD class
+		/// </summary>
+		public bool HasLanguageMaps { get; set; }
 	}
 
 	/// <summary>
@@ -44,6 +64,7 @@ namespace RA.Models.Input
 
 		/// <summary>
 		/// Task already formatted as JSON-LD
+		/// ONLY USED WITH PUBLISH LIST
 		/// </summary>
 		public MJ.Task FormattedTask { get; set; } = new MJ.Task();
 
