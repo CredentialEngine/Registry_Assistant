f6c7228f8fb064932e596aa2d1d2b216e7e22fae
Michael Parsons <58825866+mparsons-ce@users.noreply.github.com>
2025-01-23 16:24:20 -0600

    Delete src/RegistryAPI directory
    Not applicable for the public.

--- src/RegistryAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RegistryAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- src/RegistryAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RegistryAPI.WebApiApplication" Language="C#" %>
