825238f65a3b81a809b28c6af1120df9141d5aca
Michael Parsons <mparsons@credentialengine.org>
2025-04-14 13:07:00 -0500

    Add sample for Publishing Qualifications Frameworks

---
+++ src/SamplePublishing/RA.SamplesForDocumentation/QualificationsFrameworks/PublishingQualificationsFrameworks.cs
@@ -0,0 +1,161 @@
+﻿using Newtonsoft.Json;
+
+using RA.Models.Input;
+
+using System;
+using System.Collections.Generic;
+
+using APIRequest = RA.Models.Input.QualificationsFrameworkRequest;
+using APIRequestResource = RA.Models.Input.QualificationsFramework;
+
+namespace RA.SamplesForDocumentation.QualificationsFrameworks
+{
+    public class PublishingQualificationsFrameworks
+    {
+        public string Publish( string requestType = "publish" )
+        {
+            //Holds the result of the publish action
+            var result = string.Empty;
+            // Assign the api key - acquired from organization account of the organization doing the publishing
+            var apiKey = SampleServices.GetMyApiKey();
+            if ( string.IsNullOrWhiteSpace( apiKey ) )
+            {
+                //ensure you have added your apiKey to the app.config
+            }
+
+            // This is the CTID of the organization that owns the data being published
+            var organizationIdentifierFromAccountsSite = SampleServices.GetMyOrganizationCTID();
+            if ( string.IsNullOrWhiteSpace( organizationIdentifierFromAccountsSite ) )
+            {
+                //ensure you have added your organization account CTID to the app.config
+            }
+
+            //Assign a CTID for the entity being published and keep track of it
+            var myCTID = "ce-95517789-e925-4f40-ae32-8cc3007ada94";
+
+            //Populate the Qualifications Framework object
+            var myData = new APIRequestResource()
+            {
+                Name = "My Qualifications Framework Name",
+                Description = "This is some text that describes my Qualifications Framework.",
+                CTID = myCTID,
+                SubjectWebpage = "https://example.org/?t=QFramework",
+
+            };
+
+            // organization reference(s) for the resource owner
+            myData.OwnedBy.Add( new OrganizationReference()
+            {
+                CTID = organizationIdentifierFromAccountsSite
+            } );
+
+            // organization reference(s) for the resource creator
+            myData.Creator.Add( new OrganizationReference()
+            {
+                CTID = "ce-541da30c-15dd-4ead-881b-729796024b8f"
+            } );
+
+            // organization reference(s) for the resource publisher
+            myData.Publisher.Add( new OrganizationReference()
+            {
+                CTID = "ce-541da30c-15dd-4ead-881b-729796024b8f"
+            } );
+
+            //Add organization that is NOT in the credential registry
+            myData.CopyrightHolder.Add( new OrganizationReference()
+            {
+                Type = "QACredentialOrganization",
+                Name = "Name of the organization that holds the copyright for this resource",
+                SubjectWebpage = "https://example.org/SubjectWebpageUrl",
+                Description = "Description of the organization that holds the copyright for this resource."
+            } );
+
+
+            myData.CodedNotation = "qf-12345";
+            myData.DateEffective = "2020-01-01";
+            myData.ExpirationDate = "2030-12-31";
+            myData.DateCopyrighted = "2021";
+            myData.DateCreated = "2015-01-01"; ;
+            myData.DateModified = "2024-05-16"; ;
+            myData.DateValidFrom = "2020-01-01"; ;
+            myData.DateValidUntil = "2030-01-01"; ;
+
+            // Concept scheme used characterize different aspects (facets) of descriptors of nodes in a framework or similar resource.
+            myData.HasFacetScheme = new List<string>() { "ce-cc12d805-b982-4452-9534-1fd61aa584e1" };
+
+            // Terms and definitions applicable to the resource.
+            myData.HasGlossary = new List<string>() { "ce-ec963362-8cd5-40bf-89ac-710cd8dcb450", "ce-2e3d957c-dd69-4ba5-8cb7-7bdecafa5085" };
+
+            // Reference to a progression model used.
+            myData.HasProgressionModel = "ce-77f5b777-e0ce-4f9b-9e36-c821ce09b089";
+
+            myData.Identifier.Add( new IdentifierValue()
+            {
+                IdentifierTypeName = "Some Identifer For this framework",
+                IdentifierValueCode = "Catalog: xyz1234 "        //Alphanumeric string identifier of the entity
+            } );
+
+            myData.Keyword = new List<string>() { "Credentials", "Qualifications" };
+
+            myData.License = "https://example.org/licenseUrl";
+
+            myData.LifeCycleStatusType = "Active";
+
+            myData.LatestVersion = "https://example.org/LatestVersionUrl";
+            myData.PreviousVersion = "https://example.org/PreviousVersionUrl";
+            myData.NextVersion = "https://example.org/NextVersionUrl";
+            myData.SupersededBy = "https://example.org/SupersededByUrl";
+            myData.Supersedes = "https://example.org/SupersedesUrl";
+
+            myData.ProcessStandards = "https://example.org/ProcessStandardsUrl";
+            myData.ProcessStandardsDescription = "Description of the process standards related to this resource.";
+
+            myData.PublisherName = new List<string>() { "Name of the publisher" };
+            myData.PublicationStatusType ="Active";
+            myData.Rights = "Description of rights related to this resource.";
+
+            myData.Source = new List<string>() { "Name of the publisher" };
+
+            // Human-readable information resource other than a competency framework from which this resource was generated or derived by humans or machines.
+            myData.SourceDocumentation = new List<string>() { "https://example.org/SourceDocumentation" };
+            myData.Subject = new List<string>() { "Credentials", "Qualifications" };
+
+            // A CreativeWork or Event about this Thing.
+            myData.SubjectOf = new List<string>() { "https://example.org/SubjectOf" };
+
+            myData.VersionIdentifier.Add( new IdentifierValue()
+            {
+                IdentifierTypeName = "MyVersion",
+                IdentifierValueCode = "2023-09-01"        //Alphanumeric string identifier of the entity
+            } );
+            myData.VersionCode = "abc:123";
+
+            //This holds the Qualifications Framework and the identifier (CTID) for the owning organization
+            var myRequest = new APIRequest()
+            {
+                QualificationsFramework = myData,
+                DefaultLanguage = "en-US",
+                PublishForOrganizationIdentifier = organizationIdentifierFromAccountsSite
+            };
+
+            string payload = JsonConvert.SerializeObject( myRequest, SampleServices.GetJsonSettings() );
+
+            //call the Assistant API
+            SampleServices.AssistantRequestHelper req = new SampleServices.AssistantRequestHelper()
+            {
+                EndpointType = "QualificationsFramework",
+                RequestType = requestType,
+                OrganizationApiKey = apiKey,
+                CTID = myRequest.QualificationsFramework.CTID.ToLower(),   //added here for logging
+                Identifier = "testing",     //useful for logging, might use the ctid
+                InputPayload = payload
+            };
+
+            new SampleServices().PublishRequest( req );
+            //Return the result
+            return req.FormattedPayload;
+        }
+
+
+    }
+}
